76177ec11c52e2dc24df1de312671ce3aa7c59e1
Nakib A. Khan <nakib.khan1988@gmail.com>
2021-07-01 23:41:33 -0500

    (add) springboot bearer shell project

---
+++ keycloak-springboot-bearer/src/main/java/com/nakib/keycloakspringbootbearer/KeycloakSpringbootBearerApplication.java
@@ -0,0 +1,13 @@
+package com.nakib.keycloakspringbootbearer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakSpringbootBearerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakSpringbootBearerApplication.class, args);
+	}
+
+}
---
+++ keycloak-springboot-bearer/src/test/java/com/nakib/keycloakspringbootbearer/KeycloakSpringbootBearerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nakib.keycloakspringbootbearer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakSpringbootBearerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
